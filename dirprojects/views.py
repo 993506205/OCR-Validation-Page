d3d18298520af4709eeb6f78cc95d53ccda6936b
Liu Jingyu <liu.jingyu@wartsila.com>
2020-04-22 13:00:38 +0300

    authenticatied required to create new project

--- dirprojects/views.py
+++ dirprojects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import DirProjForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from functions.save2db import create_ocrfiles
 from datetime import datetime
 from .models import DirProject
@@ -9,40 +9,46 @@ from django.urls import reverse
 from django.contrib import messages
 from ocrfiles.choices import type_choices
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 
 
 def prj_create(request):
-    if request.method == "POST":
-        form = DirProjForm(request.POST)
-        files = [request.FILES.get('file[%d]' % i)
-        for i in range(0, len(request.FILES))]
-        try:
-            if form.is_valid():
-                username = None
-                creator_id = -1
-                if request.user.is_authenticated:
-                    username = request.user.username
-                    creator_id = request.user.id
-                # create dir project
-                name = form.cleaned_data['name']
-                description = form.cleaned_data['description']
-                date = datetime.strptime(datetime.now().strftime("%Y-%m-%d"), "%Y-%m-%d").date()
-                dirp = DirProject.objects.create(name=name, description=description, date=date, creator_id=creator_id)
-                pk = dirp.pk
-                #create project
-                create_ocrfiles(files, username, pk)
-                dir_projs = DirProject.objects.filter(pk=pk)
-                context = {
-                    'dir_projs': dir_projs,
-                    'type_choices': type_choices
-                }
-                messages.success(request, 'The project was created successfully!')
-                return JsonResponse({
-                    'success': True,
-                    'url': request.build_absolute_uri(reverse('index')),
-                })
-        except:
-            messages.error(request, 'Fail to create project')
-            return render(request, 'dirprojs/createnew.html', {'form': form})
-    return render(request, 'dirprojs/createnew.html', {'form': DirProjForm()})
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = DirProjForm(request.POST)
+            files = [request.FILES.get('file[%d]' % i)
+            for i in range(0, len(request.FILES))]
+            try:
+                if form.is_valid():
+                    username = None
+                    creator_id = -1
+                    if request.user.is_authenticated:
+                        username = request.user.username
+                        creator_id = request.user.id
+                    # create dir project
+                    name = form.cleaned_data['name']
+                    description = form.cleaned_data['description']
+                    date = datetime.strptime(datetime.now().strftime("%Y-%m-%d"), "%Y-%m-%d").date()
+                    dirp = DirProject.objects.create(name=name, description=description, date=date, creator_id=creator_id)
+                    pk = dirp.pk
+                    #create project
+                    create_ocrfiles(files, username, pk)
+                    dir_projs = DirProject.objects.filter(pk=pk)
+                    context = {
+                        'dir_projs': dir_projs,
+                        'type_choices': type_choices
+                    }
+                    messages.success(request, 'The project was created successfully!')
+                    return JsonResponse({
+                        'success': True,
+                        'url': request.build_absolute_uri(reverse('index')),
+                    })
+            except:
+                messages.error(request, 'Fail to create project')
+                return render(request, 'dirprojs/createnew.html', {'form': form})
+        return render(request, 'dirprojs/createnew.html', {'form': DirProjForm()})
+    else:
+        html = "<h1>Please Login to your account</h1>"
+        html += "<button onclick='goBack()'>Go Back</button><script>function goBack() {window.history.back();}</script>"
+        return HttpResponse(html)
