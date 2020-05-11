f5bf2b61db7ad5bf163bfd96d2601fac7d18d173
Liu Jingyu <liu.jingyu@wartsila.com>
2020-05-11 18:10:46 +0300

    Creating virtualenv and install needed modules

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
-echo "Creating virtualenv and installing needed modules.."
+echo "Creating virtualenv and installing needed modules..."
+echo "It will take some minutes..."
 virtualenv .env && source .env/Scripts/activate && pip install -r requirements.txt
-read -rsn1 -p"Press any key to continue..";echo
+read -rsn1 -p"Press any key to continue...";echo
