d62b2a808f0b21042e7831b8e70823b502bc4a67
Liu Jingyu <liu.jingyu@wartsila.com>
2020-05-12 15:49:50 +0300

    remove

--- startup.sh
+++
@@ -1,4 +0,0 @@
-echo "Creating virtualenv and installing needed modules..."
-echo "It will take some minutes..."
-virtualenv .env && source .env/Scripts/activate && pip install -r requirements.txt
-read -rsn1 -p"Press any key to continue...";echo
